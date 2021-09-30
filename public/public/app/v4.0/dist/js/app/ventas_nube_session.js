906fcd735b3538a715f930d9c90f3fe17be3a014
Ventas Nube <62073067+VentasNube@users.noreply.github.com>
2021-09-29 21:36:47 -0300

    Retokes en la creacion de los espacios de trabajo desde 0 quede en ahcer los doc de diseno en el usb deje las bases para que se pueda usar offine

--- public/public/app/v4.0/dist/js/app/ventas_nube_session.js
+++ public/public/app/v4.0/dist/js/app/ventas_nube_session.js
@@ -7,6 +7,7 @@
 //#############################################################################################
 //Global variables
 //##########################################################
+//Creamos la cookie donde almacenamos el workspace que abrio la session
 function createCookie(name, value, days) {
     if (days) {
         var date = new Date();
@@ -15,11 +16,11 @@ function createCookie(name, value, days) {
     } else var expires = "";
     document.cookie = name + "=" + value + expires + "; path=/account";
 }
-
+//Limpiamos la cookie por una nueva
 function eraseCookie(name) {
     createCookie(name, "", -1);
 }
-
+// Leemos la cookie para configurar la app
 function readCookie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
@@ -30,6 +31,7 @@ function readCookie(name) {
     }
     return null;
 }
+
 // Leemos la cookie seteada en el ws
 var ws_id = readCookie("ws_select");
 
@@ -41,7 +43,7 @@ if (ws_offline == 'true') {
     $('.offline_mode').removeAttr('checked'); //Cambio el atributo no chekeado
 }
 
-//Chek ws seteado
+//Chek workspace seteado
 if (!ws_id) {
     window.location = "/workspace/home";
 }
@@ -55,9 +57,8 @@ var u_db = '';
 var ws_setting_doc = '';
 var userCtx = '';
 var offline_mode = null; //Offline mode swich
-
-
 // FUNCION SESSION DE USUARIO
+/*
 async function user_session() {
     try {
         //Creo la variable session y conceto con la DB
@@ -74,8 +75,8 @@ async function user_session() {
 
         ws_setting_doc = user_db.get('ws_setting_' + ws_id, { include_docs: true, descending: true });
         offline_mode = ws_setting_doc.offline_mode
-            // console.log(ws_setting_doc.offline_mode)
-            // alert(offline_mode)
+        // console.log(ws_setting_doc.offline_mode)
+        // alert(offline_mode)
         if (offline_mode) {
             L_user_db = new PouchDB(u_db, { skip_setup: false });
             R_user_db = user_db;
@@ -114,11 +115,84 @@ async function user_session() {
             actionTextColor: "#0575e6",
             pos: 'bottom-center'
         });
-        window.location = "/login";
+       // window.location = "/login";
+    }
+}*/
+async function user_session() {
+    try {
+        //Creo la variable session y conceto con la DB
+        var _session = url_R_db + '_session';
+        _session = new PouchDB(url_R_db, { skip_setup: false });
+
+        u_session = await _session.get('_session', { include_docs: true, descending: true }); // Conecto a la USER _session
+       
+        userCtx = u_session.userCtx; //Creo la variable global
+        u_email = userCtx.name; // 
+        u_name = u_session.firstname;
+
+        u_db = 'userdb-' + nocodelog(userCtx.name); //User Db name
+        user_db = new PouchDB(url_R_db + u_db, { skip_setup: false }); // Conecto a la USER DB
+
+        ws_setting_doc = user_db.get('ws_setting_' + ws_id, { include_docs: true, descending: true });
+        offline_mode = ws_setting_doc.offline_mode
+
+        
+        // console.log(ws_setting_doc.offline_mode)
+        // alert(offline_mode)
+        if (offline_mode) {
+
+
+            L_user_db = new PouchDB(u_db, { skip_setup: false });
+            R_user_db = user_db;
+            L_user_db.sync(R_user_db, { live: true, retry: true, }); //sincronizo
+            //var session = 'http://localhost:5984/__session '; //URL global de couchDB
+
+
+            Snackbar.show({
+                text: '<span class="material-icons">wifi_off</span>  Modo Offline Activado ',
+                actionText: 'ok',
+                actionTextColor: "#0575e6",
+                pos: 'top-left',
+                duration: 50000
+            });
+            //  alert('User Modo offline');
+            // alert('Offline mode:'+offline_mode);
+        } else {
+          //  L_user_db = new PouchDB(url_R_db + u_db, { skip_setup: false });
+
+
+        }
+
+        L_user_db = new PouchDB(url_R_db + u_db, { skip_setup: false });
+
+        //Cargo la variable global user_db dependiendo si es modo offline o modo online
+        L_user_db = user_db;
+        // Creo la variable User_data con los datos de la sesion
+        this.user_data = {
+            user_db: u_db,
+            user_name: u_email,
+            user_email: u_email,
+            //user_roll: g_session.userCtx.roles,
+            url_db: url_R_db + u_db,
+            user_ws: ws_id
+        }
+        Snackbar.show({
+            text: 'Bienvenido ' + u_name + '!',
+            actionText: 'ok',
+            actionTextColor: "#0575e6",
+            pos: 'bottom-center'
+        });
+    } catch (err) {
+        Snackbar.show({
+            text: 'No estas logeado!',
+            actionText: 'ok',
+            actionTextColor: "#0575e6",
+            pos: 'bottom-center'
+        });
+       // window.location = "/login";
     }
 }
 user_session();
-
 
 async function ws_mode_offline() {
     var doc = await user_db.get('ws_setting_' + ws_id);
@@ -167,7 +241,31 @@ function logout() {
     });
 };
 
+//Boton Offline
+$(".offline_mode").click(function() {
+    if (validaForm()) {
+        const ws_offline = $(this).attr('checked'); // Tomo el parametro del atributo
+        const name = 'ws_offline'
+        if (ws_offline) {
+            $(this).removeAttr('checked'); //Cambio el atributo no chekeado
+            if (window.confirm("Quieres desactivar el modo offline?")) {
+                //window.open("exit.html", "Thanks for Visiting!");
+                createCookie(name, false);
+            }
+        } else {
+            $(this).attr('checked');
+            if (window.confirm("Quieres activar el modo offline?")) {
+                //window.open("exit.html", "Thanks for Visiting!");
+                createCookie(name, true);
+            }
+            //alert('Quieres desactivar el modo offline?');
+
+        }
+    }
+});
+
 //Funcion chekeo que esten las 2 sesiones abiertas o cierro y redirecciono al _session 
+/*
 function chek_session() {
     L_user_db.getSession(function(err, response) {
         if (err) {
@@ -214,28 +312,8 @@ function chek_session() {
         }
     });
 }
+*/
 
-$(".offline_mode").click(function() {
-    if (validaForm()) {
-        const ws_offline = $(this).attr('checked'); // Tomo el parametro del atributo
-        const name = 'ws_offline'
-        if (ws_offline) {
-            $(this).removeAttr('checked'); //Cambio el atributo no chekeado
-            if (window.confirm("Quieres desactivar el modo offline?")) {
-                //window.open("exit.html", "Thanks for Visiting!");
-                createCookie(name, false);
-            }
-        } else {
-            $(this).attr('checked');
-            if (window.confirm("Quieres activar el modo offline?")) {
-                //window.open("exit.html", "Thanks for Visiting!");
-                createCookie(name, true);
-            }
-            //alert('Quieres desactivar el modo offline?');
-
-        }
-    }
-});
 
 async function getSession() {
 
