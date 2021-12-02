76b3009254ec8cc7dafb525cd5b3d392af134d93
Ventas Nube <62073067+VentasNube@users.noreply.github.com>
2021-12-02 15:43:52 -0300

    Funcion de editar arrays

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
 
-    const version = 11111113;
+    const version = 1233;
     const expectedCaches = ['ventasnube-v-' + version];
 
       self.addEventListener('install', event => {
