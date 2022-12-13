c42904cd4173a33b43d22d96d0733af44eb992e0
Ventas Nube <62073067+VentasNube@users.noreply.github.com>
2022-12-13 15:09:11 -0300

    Botones en la edicion de precios

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
       importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
 
-      const version = 2222516224532;
+      const version = 224532;
       const expectedCaches = ['ventasnube-v-' + version];
 
       self.addEventListener('install', event => {
