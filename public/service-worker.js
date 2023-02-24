034b1efa1afa960ad54882175feb4670b0a6d80f
Ventas Nube <62073067+VentasNube@users.noreply.github.com>
2023-02-23 23:53:12 -0300

    more

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
       importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
 
-      const version = 2922222282222222112;
+      const version = 122112;
       const expectedCaches = ['ventasnube-v-' + version];
 
       self.addEventListener('install', event => {
